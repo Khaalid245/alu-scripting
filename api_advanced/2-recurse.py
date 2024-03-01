a7da810f3220ee8b1723953f536f276fd65e2323
khaalid245 <k.abdillahi@alustudent.com>
2024-03-01 17:52:12 +0200

    12

---
+++ api_advanced/2-recurse.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Script that fetch 10 hot post for a given subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
+
+    headers = {'User-Agent': 'just checking'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url,
+                headers=headers, params={'limit': 10})
+
+    if response.status_code == 200:
+
+        data = response.json().get('data', {}).get('children', [])
+
+        if data:
+                    
+                for single_data in data:
+
+                    title = single_data.get('data', {}).get('title', None)
+
+                    if title:
+                        
+
+                        print(title)
+                    else:
+                         print(None)
+
+
+
+                return title
+        else:
+             print(None)
+             return None
+
+    else:
+        return_value = None
+        print(return_value)
+        return return_value
